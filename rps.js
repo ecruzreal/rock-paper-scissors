7e5954cfe09e39e722cbfcfcbc1de9dcbf15b90d
Emmanuel Cruz Real <ecruzrea@uci.edu>
2024-08-13 15:32:50 -0700

    wrote the playRound function to determin the winner between two choices.

--- rps.js
+++ rps.js
@@ -1,4 +1,7 @@
 
+let humanScore = 0
+let computerScore = 0
+
 function getComputerChoice(){
     let num = Math.floor(Math.random() * 3)
 
@@ -28,5 +31,44 @@ function getHumanChoice(){
             return "scissors"
         }
     }
-
 }
+
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === "rock"){
+        if (computerChoice === "scissors"){
+            console.log("You win! Rock beats scissors!")
+            humanScore++
+        } else if (computerChoice === "paper"){
+            console.log("You lose! Paper beats rock!")
+            computerScore++
+        } else{
+            console.log("Draw! No points awarded!")
+        }
+    } else if (humanChoice === "paper"){
+        if(computerChoice === "scissors"){
+            console.log("You lose! Scissors beats paper!")
+            computerScore++
+         } else if(computerChoice === "rock"){
+            console.log("You win! Paper beats rock!")
+            humanScore++
+         } else{
+            console.log("Draw! No points awarded!")
+         }
+    } else{
+        if(computerChoice === "rock"){
+            console.log("You lose! Rock beats scissors!")
+            computerScore++
+        } else if (computerChoice === "paper") {
+            console.log("You win! Scissors beats paper!")
+            humanScore++
+        } else{
+            console.log("Draw! No points awarded!")
+        }
+    }
+}
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+playRound(humanSelection, computerSelection)
+
